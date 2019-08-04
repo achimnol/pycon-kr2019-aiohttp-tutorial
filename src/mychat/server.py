568c6b200444107b3f719297b46a83d2fd945777
Joongi Kim <joongi@lablup.com>
2019-08-04 23:46:02 +0900

    Make it working
     * Having one subscriber per one client SSE connection blocks non-first
       subscribers.
    
       - Also it is difficult to cleanly shutdown multiple Redis subscribers when
         shutting down the server because cancelling them makes the Redis
         connection unusable and thus renders it is impossible to issue the
         "unsubscribe" command.
    
     * Let the server have only one Redis subscriber and distribute the
       messages via local queues.
    
       - Using our own queues, we can now signal the termination by putting
         "None" as the sentinel.

--- src/mychat/server.py
+++ src/mychat/server.py
@@ -46,33 +46,57 @@ async def chat_subscribe(request: web.Request) -> web.Response:
     user_id = sess.get('user_id')
     if user_id is None:
         return web.json_response(status=401, data={'status': 'unauthorized'})
-    my_queue = asyncio.Queue()
     request_id = f'req-{secrets.token_hex(8)}'
-    app['subscribers'][request_id] = asyncio.current_task()
-    print(f'subscriber {user_id}:{request_id} started, tid={asyncio.current_task()}')
+    my_queue = asyncio.Queue()
+    app['client_queues'][request_id] = my_queue
+    print(f'subscriber {user_id}:{request_id} started')
     try:
         channels = await app['redis'].subscribe('chat')
         assert len(channels) == 1
         channel = channels[0]
         async with sse_response(request) as response:
-            async for chat_record in channel.iter():
-                chat_record = chat_record.decode('utf8')
-                print(f'subscriber {user_id}:{request_id} recv', chat_record)
+            while True:
+                chat_record = await my_queue.get()
                 if chat_record is None:
                     break
-                await response.send(chat_record)
+                await response.send(json.dumps(chat_record))
         return response
     except asyncio.CancelledError:
-        raise
-    except Exception as e:
+        raise  # let aiohttp know this handler is cancelled
+    except Exception:
         traceback.print_exc()
     finally:
         print(f'subscriber {user_id}:{request_id} terminated')
-        del app['subscribers'][request_id]
+        del app['client_queues'][request_id]
+
+
+async def chat_distribute(app: web.Application) -> None:
+    # create a separate connection dedicated to the subscriber channel
+    print('distributer started')
+    redis = await aioredis.create_redis(app['redis_addr'], db=0)
+    try:
+        channels = await redis.subscribe('chat')
+        assert len(channels) == 1
+        channel = channels[0]
+        async for chat_record in channel.iter():
+            chat_record = json.loads(chat_record.decode('utf8'))
+            for q in app['client_queues'].values():
+                q.put_nowait(chat_record)
+    except asyncio.CancelledError:
+        pass  # we know what we are doing
+    except Exception:
+        traceback.print_exc()
+    finally:
+        # Logically, we need to "unsubscribe" the channel here,
+        # but the "redis" connection is already kind-of corrupted
+        # due to cancellation.
+        # Just terminate our coroutine and let the Redis server
+        # to recognize connection close as the signal of unsubscribe.
+        print('distributer terminated')
 
 
 async def app_init(app):
-    app['subscribers'] = {}
+    app['client_queues'] = {}
     app['redis_addr'] = ('localhost', 6379)
     app['redis'] = await aioredis.create_redis_pool(app['redis_addr'], db=0)
     sess_storage = RedisStorage(
@@ -80,13 +104,15 @@ async def app_init(app):
         max_age=3600,
     )
     setup_session(app, sess_storage)
+    app['distributer'] = asyncio.create_task(chat_distribute(app))
 
 
 async def app_shutdown(app):
-    subscribers = [*app['subscribers'].values()]  # copy for safe iteration
-    for subscriber in subscribers:
-        subscriber.cancel()
-    await asyncio.gather(*subscribers)
+    client_queues = [*app['client_queues'].values()]  # copy for safe iteration
+    for q in client_queues:
+        q.put_nowait(None)
+    app['distributer'].cancel()
+    await app['distributer']
     app['redis'].close()
     await app['redis'].wait_closed()
 
